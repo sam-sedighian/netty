ece43bd214aa08206e14d1c99249edd6413ecc98
Sam Sedighian <sam.sedighian@doordash.com>
2023-02-05 19:28:22 -0800

    remove profile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,11 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import Profile from '../components/profile';
 
 export default component$(() => {
   return (
     <div class="flex h-screen w-screen items-center justify-center bg-black">
       <h1 class="my-10 text-6xl">Who's watching?</h1>
-      <div class="flex justify-center space-x-9">
-        <Profile />
-      </div>
+      <div class="flex justify-center space-x-9"></div>
     </div>
   );
 });
